bcbf45d931ec43c0b08923cff7748b4797eda6b5
aakanksha-arun <desai.aaka@northeastern.edu>
2025-12-13 12:00:15 -0500

    feat: complete core agent/server infra

---
+++ server.py
@@ -0,0 +1,30 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+#initialize
+app = FastAPI(title="Life Sync", version="1.0")
+
+@app.get("/")
+def read_root():
+    return {"message: Welcome to Life Sync!"}
+
+@app.get("/healthz")
+def health_check():
+    return {}
+
+#tools
+#dummy tool
+@app.get("/status")
+def get_status():
+    """
+    Returns the current status of the user
+    """
+    return{
+        "battery": "100%",
+        "mood": "Productive",
+        "alerts": ["Drink water"]
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
